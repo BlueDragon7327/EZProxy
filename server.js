30c682692d2f5e0ea1266b82d11cbd323c83db48
BlueDragon <150635104+BlueDragon7327@users.noreply.github.com>
2024-11-23 16:05:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Puppeteer Proxy with header spoofing and cookie setting
+// Puppeteer Proxy with header spoofing
 app.get('/proxy', async (req, res) => {
     const targetUrl = req.query.url;
     if (!targetUrl) {
@@ -34,73 +34,6 @@ app.get('/proxy', async (req, res) => {
                 },
             });
         });
-
-        // Set Poki-specific cookies if the target URL is Poki
-        if (targetUrl.includes('poki.com')) {
-            const pokiCookies = [
-                {
-                    name: 'poki_experiments',
-                    value: '%7B%22currentAB%22%3A%22c-87965a9f%22%2C%22activeTests%22%3A%5B%221482e0d9%22%2C%222316fb42%22%2C%2246f40048%22%2C%2275cf7192%22%2C%227b494404%22%2C%2287965a9f%22%2C%228d1608d2%22%2C%22959a0db8%22%2C%22a33ffd62%22%2C%22be7bb646%22%5D%7D',
-                    domain: 'poki.com',
-                    path: '/',
-                    expires: new Date('2025-05-22T16:56:29Z').getTime() / 1000,
-                    httpOnly: false,
-                    secure: true,
-                    sameSite: 'Lax',
-                },
-                {
-                    name: 'poki_uid',
-                    value: 'Z0JPjRcRUgW0tPQrl4NR0g',
-                    domain: 'poki.com',
-                    path: '/',
-                    expires: new Date('2025-05-22T16:56:29Z').getTime() / 1000,
-                    httpOnly: false,
-                    secure: false,
-                    sameSite: 'Lax',
-                },
-                {
-                    name: 'poki_uid_new',
-                    value: '1',
-                    domain: 'poki.com',
-                    path: '/',
-                    expires: new Date('2025-05-22T16:56:29Z').getTime() / 1000,
-                    httpOnly: false,
-                    secure: false,
-                    sameSite: 'Lax',
-                },
-                {
-                    name: 'poki_uid_version',
-                    value: '1',
-                    domain: 'poki.com',
-                    path: '/',
-                    expires: new Date('2025-05-22T16:56:29Z').getTime() / 1000,
-                    httpOnly: false,
-                    secure: false,
-                    sameSite: 'Lax',
-                },
-                {
-                    name: 'poki_uid_ttl',
-                    value: '15552000',
-                    domain: 'poki.com',
-                    path: '/',
-                    expires: new Date('2025-05-22T16:56:29Z').getTime() / 1000,
-                    httpOnly: false,
-                    secure: false,
-                    sameSite: 'Lax',
-                },
-                {
-                    name: 'poki_session',
-                    value: '{"id":"Z0JPjRlmairFa0axO6I65g","expire":1732400789534,"tab_id":"Z0JPjX3LnKa8o0cQPNmw2A","depth":1,"count":1,"page":{"path":"/","type":"home","id":0,"start":1732398989256,"pageview_id":"Z0JPjeZJHsHXvuNDzBOm4Q"},"previous_page":{},"landing_page":{"path":"/","type":"home","id":0,"start":1732398989256,"pageview_id":"Z0JPjeZJHsHXvuNDzBOm4Q"},"referrer_domain":""}',
-                    domain: 'poki.com',
-                    path: '/',
-                    expires: new Date('2025-05-22T16:56:29Z').getTime() / 1000,
-                    httpOnly: false,
-                    secure: false,
-                    sameSite: 'Lax',
-                },
-            ];
-            await page.setCookie(...pokiCookies);
-        }
 
         // Navigate to the target URL
         await page.goto(targetUrl, { waitUntil: 'networkidle2' });
